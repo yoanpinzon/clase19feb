835c9bad262679edc26e48d88e0fad818457b518
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-19 08:40:27 -0500

    templates (plantillas)

--- main.cpp
+++ main.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
+template< typename T>
+T suma( T x, T y ) {
+    return x + y;
+}
+
 int main() {
-    //int *b = new int[5]{1,4,7,8,9};
-    int b[] = {1,4,7,8,9};
-    for ( int i = 0; i < 5; i++ ) {
-        cout << b[ i ] << endl;
-    }
-    //delete[] b;
+    int a = 4, b = 8;
+    float e = 4.4, f = 9.9;
+    string m{"julia"}, n{"ernestina"};
+    cout << suma( a, b ) << endl;
+    cout << suma( e, f ) << endl;
+    cout << suma( m, n ) << endl;
     return 0;
 }
