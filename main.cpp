53f1e1c3da8824c8879c593ce0f8018121ad95b3
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-19 07:50:56 -0500

    auto

--- main.cpp
+++ main.cpp
@@ -3,30 +3,9 @@
 using namespace std;
 
 int main() {
-    int c[100] = {1, 2, 4};
-    int b[] = {3, 4, 5, 7, 8, 9};
-    int a[8];
-    a[0] = 9;
-    a[1] = 19;
-    a[2] = 8;
-    a[3] = 3;
-    a[4] = 12;
-    a[5] = 99;
-    a[6] = 109;
-    a[7] = 33;
-    int s{0};
-    for (int i = 0; i < 8; i++) {
-        cout << a[i] << endl;
-        s += a[i];
-    }
-    cout << endl;
-    for (int i = 0; i < 6; i++) {
-        cout << b[i] << endl;
-    }
-    cout << "suma = " << s << endl;
-    cout << endl;
-    for (int i = 0; i < 100; i++) {
-        cout << "c[" << i << "]=" << c[i] << endl;
+    string a[] = {"jose", "juan", "maria" };
+    for ( auto i : a ) {
+        cout << i << endl;
     }
     return 0;
 }
