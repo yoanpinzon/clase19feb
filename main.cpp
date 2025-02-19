159dc65ed384025390d5dbe4aaee122565051cd4
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-19 08:12:56 -0500

    referencias

--- main.cpp
+++ main.cpp
@@ -3,16 +3,13 @@
 using namespace std;
 
 int main() {
-    int a[] = {4, 8, 9, 24, 55, 12 };
-    int i = 16;
-    int *pi;
-    int *maria;
-    pi = &i;
-    maria = pi;
-    cout << *pi << endl;
-    *maria+=8;
-    cout << i << endl;
-    cout << *(a-5) << endl;
-    cout << a[5] << endl;
+    int a = 33;
+    int &b = a;
+    int c = 0xF;
+    b = c;
+    cout << a++ << endl;
+    cout << b << endl;
+    cout << c << endl;
+
     return 0;
 }
