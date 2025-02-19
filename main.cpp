6ca70ead30416843d384e0519cbd8e36818b9572
Profesorcito <yoan.pinzon@javerianacali.edu.co>
2025-02-19 08:08:08 -0500

    apuntadores

--- main.cpp
+++ main.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 
 int main() {
-    string a[] = {"jose", "juan", "maria" };
-    for ( auto i : a ) {
-        cout << i << endl;
-    }
+    int a[] = {4, 8, 9, 24, 55, 12 };
+    int i = 16;
+    int *pi;
+    int *maria;
+    pi = &i;
+    maria = pi;
+    cout << *pi << endl;
+    *maria+=8;
+    cout << i << endl;
+    cout << *(a-5) << endl;
+    cout << a[5] << endl;
     return 0;
 }
